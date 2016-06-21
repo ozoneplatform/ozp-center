14fe40fbf946eb40f1eb7938fd95eb72bc323a83
rkenyon1969 <rjkenyon@gmail.com>
2016-06-21 11:12:32 -0400

    Fix(Bookmark): #649: Added jS code to polyfill str.includes

--- app/js/main.js
+++ app/js/main.js
@@ -131,3 +131,18 @@ We have detected that you are using an unsupported browser and some features may
 `);
 window.open(IE_REDIRECT_URL);
 }
+
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+    if (typeof start !== 'number') {
+      start = 0;
+    }
+
+    if (start + search.length > this.length) {
+      return false;
+    } else {
+      return this.indexOf(search, start) !== -1;
+    }
+  };
+}
