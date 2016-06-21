411667f1ff689e307c63cf734c81c3dfb7fdd9ca
rkenyon1969 <rjkenyon@gmail.com>
2016-06-21 13:41:30 -0400

    Fix(Bookmark): #649: I like Matt's better too

--- app/js/main.js
+++ app/js/main.js
@@ -132,17 +132,8 @@ We have detected that you are using an unsupported browser and some features may
 window.open(IE_REDIRECT_URL);
 }
 
-if (!String.prototype.includes) {
-  String.prototype.includes = function(search, start) {
-    'use strict';
-    if (typeof start !== 'number') {
-      start = 0;
-    }
-
-    if (start + search.length > this.length) {
-      return false;
-    } else {
-      return this.indexOf(search, start) !== -1;
-    }
+if (!String.includes) {
+  String.prototype.includes = function(value) {
+    return this.valueOf().indexOf(value) > - 1;
   };
 }
