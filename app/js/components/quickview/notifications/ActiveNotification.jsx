b997d8a554d0f341b46f398c9ad1531c16e87212
Andrew Konsowski <akonsow@oslo.adet-131.ambulate-dev.bia.com>
2016-07-08 08:50:45 -0400

    fix(notifications):changed wording for active notifications from expired to expires

--- app/js/components/quickview/notifications/ActiveNotification.jsx
+++ app/js/components/quickview/notifications/ActiveNotification.jsx
@@ -32,7 +32,7 @@ var ActiveNotification = React.createClass({
             <div className="PastNotification">
                 <div className="PastNotification__Header">
                     <h5 style={{margin: 0, fontWeight: 400}}>{(listing) ? listing.title : 'AppsMall'}</h5>
-                    <em>Expired: <_Date date={expiresDate} /> at <Time date={expiresDate} /></em>
+                    <em>Expires: <_Date date={expiresDate} /> at <Time date={expiresDate} /></em>
                 </div>
                 <p>{ message }</p>
             </div>
