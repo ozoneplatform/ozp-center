2cd1a1d89ffc4fdca0f3b5da061523b13c3afd88
Michael Lee <michael.lee@bia-boeing.com>
2017-03-27 16:00:58 -0400

    JIRA-247: Display errors on front-end for ElasticSearch

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
         ],
         "no-undef": 0,
         "quotes": 0,
-        "no-unused-vars": 0
+        "no-unused-vars": 0,
+        "no-console": 0
     },
     "env": {
         "es6": true,
