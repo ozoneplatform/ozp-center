3ef6f4b322684c4de7517f58216c817457b50e73
Jason Fiddle <jason.fiddle@bia-boeing.com>
2017-06-05 16:09:42 -0400

    fix: Selecting the center icon now takes the user to the Center Homepage.

--- app/js/components/NavBar/index.jsx
+++ app/js/components/NavBar/index.jsx
@@ -50,7 +50,7 @@ var NavBar = React.createClass({
                     <div className="navbar-left">
                         <ul className="nav navbar-nav">
                             <li id="tourstop-hud" className="tooltiped" data-toggle="tooltip" data-placement="bottom" title="HUD"><a className="lrg" href={HUD_URL}><i className="icon-home-grayLightest"></i><span className="hidden-span">HUD</span></a></li>
-                            <li id="tourstop-center" className="active tooltiped" data-toggle="tooltip" data-placement="bottom" title="Center"><a className="lrg" href='#/home' ><i className="icon-shopping-grayLightest activeIcon"></i><span className="hidden-span">Center</span></a></li>
+                            <li id="tourstop-center" className="active tooltiped" data-toggle="tooltip" data-placement="bottom" title="Center"><a className="lrg" href='' ><i className="icon-shopping-grayLightest activeIcon"></i><span className="hidden-span">Center</span></a></li>
                             { !secondParty &&
                               <li id="tourstop-webtop" className="tooltiped" data-toggle="tooltip" data-placement="bottom" title="Webtop"><a className="lrg" href={WEBTOP_URL}><i className="icon-layout-grayLightest"></i><span className="hidden-span">Webtop</span></a></li>
                             }
