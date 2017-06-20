5d778201c77766a8edb20f886c2330e8cb82797b
Michael Lee <michael.lee@bia-boeing.com>
2017-06-20 08:48:17 -0400

    Show the creation date when displaying notifications

--- app/js/components/management/mall/notifications/PastNotification.jsx
+++ app/js/components/management/mall/notifications/PastNotification.jsx
@@ -15,7 +15,8 @@ var PastNotification = React.createClass({
         }
     },
     render() {
-        var { listing, expiresDate, message } = this.props.notification;
+        var { listing, expiresDate, message, createdDate } = this.props.notification;
+        var created = new Date(createdDate);
         var e = document.createElement('div');
         e.innerHTML = message;
 
@@ -23,6 +24,7 @@ var PastNotification = React.createClass({
             <div className="PastNotification">
                 <div className="PastNotification__Header">
                     <h5 style={{margin: 0, fontWeight: 400}}>{(listing) ? listing.title : 'AppsMall' }</h5>
+                    <em>Created: <_Date date={created} /> at <Time date={created} /> / </em>
                     <em>Expired: <_Date date={expiresDate} /> at <Time date={expiresDate} /></em>
                 </div>
                 <p dangerouslySetInnerHTML={{ __html: message}} />
