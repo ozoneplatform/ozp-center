30f7658b748b7551047370ea46bc475220d1c7d5
flesher1 <evan.flesher@bia-boeing.com>
2017-11-09 12:24:06 -0500

    feat(activity log): add reviews to activity log
    Add log entry to Recent Activity tab in Listing Management.
    
    Closes AMLNG-786

--- app/js/components/management/user/RecentActivity.jsx
+++ app/js/components/management/user/RecentActivity.jsx
@@ -67,6 +67,7 @@ var RecentActivity = React.createClass({
                 'DISABLED' : 'View',
                 'CREATED' : 'View Draft',
                 'APPROVED_ORG' : 'Review Listing',
+                'REVIEWED' : 'View',
                 'REVIEW_EDITED' : 'View',
                 'REVIEW_DELETED' : 'View',
                 'PENDING_DELETION' : 'View'
