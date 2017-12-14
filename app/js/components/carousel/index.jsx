9caedd606f2a2f73f3963a637f194171e9031f17
flesher1 <evan.flesher@bia-boeing.com>
2017-12-14 08:48:10 -0500

    refactor(carousel): create arrows when needed
    Adds functionality to prevent carousel from displaying arrows when all
    the tiles will fit on screen.
    Small modifications to animation logic.
    
    Closes #AMLNG-842

--- app/js/components/carousel/index.jsx
+++ app/js/components/carousel/index.jsx
@@ -41,18 +41,18 @@ var Carousel = React.createClass({
         // if (this._currentElement.ref==="featured"){
         //     return {slides: this.props.slides.reverse()  };
         // }
-        return {slides: this.props.slides  };
+        return {slides: this.props.slides, showArrows : true};
     },
 
     goToPrevSlide: function (e) {
         e.preventDefault();
         var me = this;
-        var img_width = $(e.target).nextAll('ul').find('img').width() + 30;
+        var img_width = ((this.refs.slides.getDOMNode()).getBoundingClientRect()).width + 30;
         let { slides } = me.state;
         let slidesLength = slides.length;
         var temp = slides.splice(0, slidesLength-1);
         me.setState({slides: slides.concat(temp)});
-        $(e.target).nextAll('ul').animate({
+        $(this.refs.slideList.getDOMNode()).animate({
             "margin-left": '-=' + img_width
         }, 0, function() {
             $(this).animate({
@@ -65,8 +65,8 @@ var Carousel = React.createClass({
     goToNextSlide: function (e) {
         e.preventDefault();
         var me = this;
-        var img_width = $(e.target).next('ul').find('img').width() + 30;
-        $(e.target).next('ul').animate({
+        var img_width = ((this.refs.slides.getDOMNode()).getBoundingClientRect()).width + 30;
+        $(this.refs.slideList.getDOMNode()).animate({
             "margin-left": '-=' + img_width
         }, 500, function() {
             let { slides } = me.state;
@@ -84,24 +84,53 @@ var Carousel = React.createClass({
         this.setState({slides: slides});
     },
 
+    checkArrows: function() {
+    //This will make arrows go away if there are fewer listings than screen size.
+        var space = ((this.refs.slideList.getDOMNode()).getBoundingClientRect()).width;
+        var slide = ((this.refs.slides.getDOMNode()).getBoundingClientRect()).width + 30;
+        slide = slide * this.state.slides.length;
+        if(slide < space){
+            this.setState({
+                showArrows : false
+            })
+        }else{
+            this.setState({
+                showArrows : true
+            })
+        }
+    },
+
+    componentDidMount: function() {
+        var me = this;
+        $( window ).resize(function(){
+            me.checkArrows()
+        });
+        this.checkArrows();
+    },
+
     render: function() {
         var items;
+        var showArrows = this.state.showArrows;
         var classes = "carousel__slide" + (this.state.slides[0].type.displayName==="FeaturedListingTile" ? " carousel__slide--featured" : "");
 
         items = this.state.slides.map((slide, index) =>
-            <div className={classes}>
+            <div className={classes} ref="slides">
                 {slide}
             </div>
         );
 
         return (
-            <div className="carousel">
-                {items.length === 1 ?
-                null : <CarouselLeftArrow onClick={e => this.goToPrevSlide(e)} /> }
-                {items.length === 1 ?
-                null : <CarouselRightArrow onClick={e => this.goToNextSlide(e)} /> }
+            <div className="carousel" onResize={() => console.log('resized!')}>
+                {showArrows ?
+                    <div>
+                        {items.length === 1 ?
+                            null : <CarouselLeftArrow onClick={e => this.goToPrevSlide(e)} /> }
+                        {items.length === 1 ?
+                            null : <CarouselRightArrow onClick={e => this.goToNextSlide(e)} /> }
+                    </div> : null
+                }
 
-                <ul className="carousel__slides">
+                <ul className="carousel__slides" ref="slideList">
                     {items}
                 </ul>
 
