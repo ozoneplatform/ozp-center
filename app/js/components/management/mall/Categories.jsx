0e7682d247192a1b39a33c9ab06d15f812a19af0
cwilliams2017 <32687060+cwilliams2017@users.noreply.github.com>
2018-03-29 11:46:12 -0400

    20180327 validate category (#943)
    * feat(category): Validation Button
    
    * feat(category): Validation Button toggle
    
    * feat(category): Validation Button update hint

--- app/js/components/management/mall/Categories.jsx
+++ app/js/components/management/mall/Categories.jsx
@@ -39,10 +39,45 @@ var Categories = React.createClass({
                 }
             },
             grid: {
+                name: 'category',
                 columns: [
                     { field: 'title', caption: 'Title', size: '50%' },
                     { field: 'description', caption: 'Description', size: '50%' }
-                ]
+                ],
+                show: {
+                    toolbar: true,
+                    toolbarAdd: true,
+                    toolbarEdit: true,
+                    toolbarDelete: true,
+                    toolbarValidate: true,
+                    toolbarSearch: false,
+                    toolbarReload: false,
+                    toolbarColumns: false
+                },
+
+                onClick: function (event) {
+                    event.onComplete = function() {
+                        var sel = this.getSelection();
+                        var record = this.get( sel["0"])
+                        if (this.getSelection().length > 0)
+                          { w2ui['category_toolbar'].enable('validate') } 
+                        else 
+                          { w2ui['category_toolbar'].disable('validate') }
+                        }
+
+                    },
+                    toolbar:{
+                        items: [
+                            { type: 'break'},
+                            { type: 'button', id :'validate', caption: 'Validate', img: 'icon-folder', hint: w2utils.lang('Validate selected record'), disabled:true }
+                        ], 
+                        onClick: function (event) {
+                            var sel = w2ui["category"].getSelection(); 
+                            var record =  w2ui["category"].get(sel["0"]);
+                           if (event.target == 'validate') { w2alert('<h4>This feature is coming soon!</h4>'+ '</br></br>Validation information for the <em>' + record.title + '</em> category' , 'Validate Category');}
+                        }
+                    }
+
             }
         };
     },
